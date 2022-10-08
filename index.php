aacdb38d6b2be584b12ad3caf923dffa17bc18cd
William Bouet <william.bouet.14@gmail.com>
2022-10-08 16:26:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'Bicycle.php';
+require 'Car.php';
+require 'Truck.php';
+
+/* $bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+ */
+
+$camion = new Truck(100, 'rouge', 4, 'fuel');
+
+
